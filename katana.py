41211308cc04262c2afb6a899d0cafde0c614add
Sebastien Estienne <sebastien.estienne@gmail.com>
2013-02-25 13:40:11 +0100

    katana: add support for revalidation of the cache

--- katana.py
+++ katana.py
@@ -21,7 +21,7 @@ USER_AGENT = 'Katana/%s' % __version__
 @contextmanager
 def wlock(filename):
     try:
-        with open(filename, 'r') as lock:
+        with open(filename, 'rb+') as lock:
             while True:
                 try:
                     fcntl.flock(lock, fcntl.LOCK_SH | fcntl.LOCK_NB)
@@ -32,14 +32,14 @@ def wlock(filename):
                     else:
                         raise
                 else:
-                    yield
+                    yield False, lock
                     break
     except IOError as exc:
         if exc.errno == errno.ENOENT:
-            with open(filename, 'wb') as lockw:
+            with open(filename, 'wb') as lock:
                 while True:
                     try:
-                        fcntl.flock(lockw, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                        fcntl.flock(lock, fcntl.LOCK_EX | fcntl.LOCK_NB)
                     except IOError as exc:
                         if exc.errno == errno.EAGAIN:
                             gevent.sleep(0.05)
@@ -47,12 +47,12 @@ def wlock(filename):
                         else:
                             raise
                     else:
-                        yield lockw
+                        yield True, lock
 
                         if os.path.exists(filename):
-                            if not lockw.closed:
-                                lockw.seek(0, 2)
-                                if not lockw.tell():
+                            if not lock.closed:
+                                lock.seek(0, 2)
+                                if not lock.tell():
                                     os.unlink(filename)
                             elif not os.path.getsize(filename):
                                 os.unlink(filename)
@@ -67,6 +67,9 @@ class Timer:
 
     def __str__(self):
         return '%.3f ms' % ((time() - self.start) * 1000)
+
+
+META_MAGIC = 'A'
 
 
 class Katana(object):
@@ -84,7 +87,7 @@ class Katana(object):
             'thumb_max_height': 1080,
             'thumb_max_quality': 90,
             'thumb_default_quality': 75,
-            'origin_fetch_timeout': 1,
+            'origin_fetch_timeout': 3,
             'origin_timeout': 1,
             'origin_mapping': {
                 'localhost': {'ips': ['127.0.0.1'], 'headers': {'Host': 'localhost'}}
@@ -96,6 +99,8 @@ class Katana(object):
             'proxy_url_re': None,
             'proxy_origin': '',
             'proxy_cache_path': '',
+            'cache_force_expires': False,
+            'cache_default_expires': 600,
             'cache_dir': './data',
             'cache_dir_max_usage': 90,
             'clean_older_than': 24,
@@ -113,6 +118,45 @@ class Katana(object):
         for name, conf in self.config['origin_mapping'].items():
             self.hws.set_cluster(name, conf['ips'], conf.get('headers'))
 
+    def get_meta(self, cache):
+        try:
+            with open('%s.meta' % cache, 'r') as cache_meta:
+                splitted = cache_meta.read().split('|')
+                if splitted[0] == META_MAGIC:
+                    magic, expires, last_modified, etag = splitted
+                    return {
+                        'expires': int(expires),
+                        'last_modified': last_modified,
+                        'etag': etag,
+                    }
+                else:
+                    self.slog.error('get_meta wrong magic [%s] for %s', splitted[0], cache)
+                    os.remove('%s.meta' % cache)
+        except (IOError, OSError) as exc:
+            self.slog.error('get_meta failed for %s: %s', cache, exc)
+        return {}
+
+    def set_meta(self, cache, headers):
+        try:
+            with open('%s.meta' % cache, 'w') as cache_meta:
+                etag = headers.get('etag', '')
+                last_modified = headers.get('last-modified', '')
+                expires = self.config['cache_default_expires']
+                if not self.config['cache_force_expires']:
+                    m = re.match('.*max-age=(\d+).*', headers.get('cache-control', ''))
+                    if m:
+                        expires = int(m.group(1))
+                expires = int(time() + expires)
+                cache_meta.write('%s|%s|%s|%s' % (META_MAGIC, expires, last_modified, etag))
+                return {
+                    'expires': expires,
+                    'last_modified': last_modified,
+                    'etag': etag,
+                }
+        except (IOError, OSError) as exc:
+            self.slog.error('set_meta failed for %s: %s', cache, exc)
+        return {}
+
     def _get_cache(self, cache):
         if os.path.exists(cache):
             if os.path.getsize(cache):
@@ -128,42 +172,50 @@ class Katana(object):
             if exc.errno not in (errno.EEXIST, errno.ENOENT):
                 raise
 
-        with wlock(cache) as cache_fdw:
-            if cache_fdw:
+        with wlock(cache) as (write, cache_fd):
+            meta = self.get_meta(cache)
+            if 'expires' in meta:
+                if time() > meta['expires']:
+                    write = True
+            if write:
                 self.slog.debug('write lock: resolving %s', origin)
-                info = self.hws.resolve(origin)
+                info = self.hws.resolve(origin, etag=meta.get('etag'), last_modified=meta.get('last_modified'))
                 if info:
-                    url = info[1]
+                    url = info['url']
+                    if not info['modified']:
+                        self.slog.debug('url=%s not modified', url)
+                        return cache, self.set_meta(cache, info['headers'])
                     headers = {'User-Agent': USER_AGENT}
-                    if info[2]:
-                        headers['Host'] = info[2]
+                    if info['host']:
+                        headers['Host'] = info['host']
                     try:
                         req = urllib2.Request(url, headers=headers)
-                        req = urllib2.urlopen(req, timeout=self.config['origin_fetch_timeout'])
+                        resp = urllib2.urlopen(req, timeout=self.config['origin_fetch_timeout'])
                     except urllib2.HTTPError as exc:
                         self.slog.error('fetching url=%s error: %s', url, exc)
                     except Exception as exc:
                         self.slog.exception('fetching url=%s failed', url)
                     else:
+                        meta = self.set_meta(cache, dict(resp.headers))
                         while True:
-                            chunk = req.read(self.config['chunk_size'])
+                            chunk = resp.read(self.config['chunk_size'])
                             if not chunk:
                                 break
-                            cache_fdw.write(chunk)
+                            cache_fd.write(chunk)
                         self.slog.debug('fetched %s to %s', url, cache)
-                        return cache
+                        return cache, meta
                 else:
                     self.slog.debug('%s not found on origin', cache)
 
             elif self._get_cache(cache):
                 self.slog.debug('read lock: %s found in cache as %s', origin, cache)
-                return cache
+                return cache, meta
 
-        return None
+        return None, {}
 
     def get_image_resized(self, image_src, cache, width, height, fit, quality, values):
-        with wlock(cache) as lockw:
-            if lockw:
+        with wlock(cache) as (write, cache_fd):
+            if write:
                 if not resize(image_src, cache, width, height, fit, quality):
                     return None
 
@@ -213,12 +265,12 @@ class Katana(object):
 
         origin = self.config['resize_origin'].format(**values)
         cache = '%s%s' % (self.config['cache_dir'], self.config['resize_cache_path_source'].format(**values))
-        image_src = self.get_file(origin, cache)
+        image_src, meta = self.get_file(origin, cache)
         if image_src:
             os.utime(image_src, None)
             cache = '%s%s' % (self.config['cache_dir'], self.config['resize_cache_path_resized'].format(**values))
             return self.get_image_resized(image_src, cache, width, height, fit, quality, values)
-        return None
+        return None, meta
 
     def proxy(self, values):
         origin = self.config['proxy_origin'].format(**values)
@@ -238,9 +290,13 @@ class Katana(object):
             if regex:
                 match = re.match(regex, environ['PATH_INFO'])
                 if match:
-                    image_dst = getattr(self, action)(match.groupdict())
+                    image_dst, meta = getattr(self, action)(match.groupdict())
                     if image_dst:
                         headers = [('Content-Type', 'image/jpeg'), ('X-Response-Time', str(timer)),]
+                        if meta.get('etag'):
+                            headers.append(('ETag', meta['etag']))
+                        if meta.get('last_modified'):
+                            headers.append(('Last-Modified', meta['last_modified']))
                         if self.config['accel_redirect']:
                             accel_redirect = self.config['accel_redirect_path'] + image_dst[len(self.config['cache_dir']):]
                             headers.append(('X-Accel-Redirect', accel_redirect))
