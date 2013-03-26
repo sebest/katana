1fa7b4785ed42e2afd73da2105dd690f01b9a774
Sebastien Estienne <sebastien.estienne@gmail.com>
2013-03-26 17:34:57 +0100

    improve packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from distutils.core import setup
+
+from katana import __version__
+
 setup(
     name='katana',
-    version='1.0',
+    version=__version__,
+    author='Sebastien Estienne',
+    author_email='sebastien.estienne@gmail.com',
+    url='-',
     py_modules=['katana'],
     install_requires=['gevent', 'gunicorn', 'PIL'],
     )
