42c3d5d470e61241abe15c353839d02c8ab13106
Sebastien Estienne <sebastien.estienne@gmail.com>
2014-10-08 21:43:09 -0700

    improve debugging and no resize

--- katana/resizer.py
+++ katana/resizer.py
@@ -16,19 +16,21 @@ def resize(src, dst, width=0, height=0, fit=True, quality=75):
     if origin_height == origin_width == 0:
         return False
 
-    if not width:
+    if not width and height:
         width = int(ceil(origin_width / (float(origin_height) / height)))
-    if not height:
+    if not height and width:
         height = int(ceil(origin_height / (float(origin_width) / width)))
 
-    logger.debug('resize %s: (%dx%d) => (%dx%d) mode=%s quality=%d', src, origin_width, origin_height, width, height, 'fit' if fit else 'fill', quality)
-
     if width != 0 and height != 0:
+        logger.debug('resize %s: (%dx%d) => (%dx%d) mode=%s quality=%d', src, origin_width, origin_height, width, height, 'fit' if fit else 'fill', quality)
         if fit:
             img = ImageOps.fit(img, (width, height), Image.ANTIALIAS)
         else:
             img = img.copy()
             img.thumbnail((width, height), Image.ANTIALIAS)
+    else:
+        logger.debug('no resize %s: (%dx%d) quality=%d', src, origin_width, origin_height, quality)
+
     try:
         if img.mode != "RGB":
             img = img.convert("RGB")
